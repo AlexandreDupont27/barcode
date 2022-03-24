c02c718e6eb508e105e5a39c6dea278a953a89a0
Alexandre Dupont <adupont@nmedia.ca>
2022-03-24 12:14:19 -0400

    Ajout du dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'BarcodeProject', // TO DO
+    title: 'barcode', // TO DO
     htmlAttrs: {
       lang: 'fr', // TO DO
     },
